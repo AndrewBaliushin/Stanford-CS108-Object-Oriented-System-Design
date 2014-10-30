8f60e795fc45534b0c0ee784232eaaefd862b83c
Andrew Baliushin <uo4ever@mail.ru>
2014-10-30 16:05:06 +0400

    Board and test for it

--- HW2Tetris/tetris/Board.java
+++ HW2Tetris/tetris/Board.java
@@ -1,6 +1,10 @@
 // Board.java
 package tetris;
 
+import java.util.Arrays;
+
+import javax.management.RuntimeErrorException;
+
 /**
  CS108 Tetris Board.
  Represents a Tetris board -- essentially a 2-d grid
@@ -17,6 +21,31 @@ public class Board	{
 	private boolean DEBUG = true;
 	boolean committed;
 	
+	/* my variables */
+	
+	/* stores how tall columns are. 0 if no blocks in column
+	 * 2 if 2 blocks. etc
+	 * starts from x=0; So {0,1,2,0} is
+	 * flat-1block-2block-flat 
+	 */
+	private int heightsOfCols[];
+	
+	/*
+	 * Stores how many blocks are in each row starting from bottom
+	 * {5,3,2} means 5 in first, 3 in second and 2 in last row
+	 */
+	private int widthsOfRows[];
+	
+	/*
+	 * We can call clearRows() after place() even if committed == false;
+	 * place() turn this to true; commit() turns to false;
+	 */
+	private boolean placeMethodUsed;
+
+	/* backups for undo*/
+	private boolean[][] buGrid;
+	private int buHeightsOfCols[];
+	private int buWidthsOfRows[];
 	
 	// Here a few trivial methods are provided:
 	
@@ -31,6 +60,13 @@ public class Board	{
 		committed = true;
 		
 		// YOUR CODE HERE
+		heightsOfCols = new int[width];
+		widthsOfRows = new int[height];
+		
+		//init backup storage
+		buGrid = new boolean[width][height];
+		buHeightsOfCols = new int[width];
+		buWidthsOfRows = new int[height];
 	}
 	
 	
@@ -49,24 +85,71 @@ public class Board	{
 		return height;
 	}
 	
+	/**
+	 * Fills heightsOfCols[] and widthOfRows[]
+	 */
+	private void makeHeightAndWidthArrays() {
+		//clear arrays
+		Arrays.fill(heightsOfCols, 0);
+		Arrays.fill(widthsOfRows, 0);
+		
+		for (int x = 0; x < grid.length; x++) {
+			for (int y = 0; y < grid[x].length; y++) {
+				if (grid[x][y]) { //empty cell
+					heightsOfCols[x] = y + 1; //cause 0 means flat. 
+					widthsOfRows[y]++;
+				}
+			}
+		}
+	}
 	
 	/**
 	 Returns the max column height present in the board.
 	 For an empty board this is 0.
 	*/
 	public int getMaxHeight() {	 
-		return 0; // YOUR CODE HERE
+		int output = 0;
+		for (int i = 0; i < heightsOfCols.length; i++) {
+			if (heightsOfCols[i] > output) output = heightsOfCols[i];
+		}
+		
+		return output;
 	}
 	
 	
 	/**
 	 Checks the board for internal consistency -- used
 	 for debugging.
+	 
+	 Checks that widthsOfRows, heightsOfCols and maxHeight are correct.
 	*/
 	public void sanityCheck() {
-		if (DEBUG) {
-			// YOUR CODE HERE
+		if (!DEBUG) {
+			return;
 		}
+		
+		int[] checkWidths = new int[height];
+		int[] checkHeights = new int[width];
+		int checkMaxHeight = 0;
+		
+		for (int x = 0; x < grid.length; x++) {
+			for (int y = 0; y < grid[x].length; y++) {
+				if (grid[x][y]) { // empty cell
+					checkHeights[x] = y + 1; // cause 0 means flat.
+					checkWidths[y]++;
+					if ((y + 1) > checkMaxHeight) {
+						checkMaxHeight = y + 1;
+					}
+				} 
+			}
+		}
+		
+		if (!Arrays.equals(checkHeights, heightsOfCols) || 
+				!Arrays.equals(checkWidths, widthsOfRows) ||
+				checkMaxHeight != getMaxHeight()) {
+			throw new RuntimeException("sanityCheck() failed");
+		}
+
 	}
 	
 	/**
@@ -79,7 +162,21 @@ public class Board	{
 	 to compute this fast -- O(skirt length).
 	*/
 	public int dropHeight(Piece piece, int x) {
-		return 0; // YOUR CODE HERE
+		int[] skirt = piece.getSkirt(); //exmpl: for S-piece it's {1,0} 
+		
+		if (x < 0 || (x + skirt.length) > width) {
+			throw new RuntimeException("droping out of bounds");
+		}
+		
+		int finalRestHeight = 0;
+		for (int i = x; i < x + skirt.length; i++) {
+			int currentHeight = getColumnHeight(i) + skirt[i];
+			if (currentHeight > finalRestHeight) {
+				finalRestHeight = currentHeight;
+			}
+		}
+		
+		return finalRestHeight;
 	}
 	
 	
@@ -89,7 +186,7 @@ public class Board	{
 	 The height is 0 if the column contains no blocks.
 	*/
 	public int getColumnHeight(int x) {
-		return 0; // YOUR CODE HERE
+		return heightsOfCols[x]; // YOUR CODE HERE
 	}
 	
 	
@@ -98,7 +195,7 @@ public class Board	{
 	 the given row.
 	*/
 	public int getRowWidth(int y) {
-		 return 0; // YOUR CODE HERE
+		 return widthsOfRows[y]; // YOUR CODE HERE
 	}
 	
 	
@@ -108,7 +205,15 @@ public class Board	{
 	 always return true.
 	*/
 	public boolean getGrid(int x, int y) {
-		return false; // YOUR CODE HERE
+		if (x >= width || x < 0 || y >= height || y < 0) {
+			return true;
+		} else if (grid[x][y] == true) {
+			return true;
+		} else {
+			return false;
+		}
+		
+		//return false; // YOUR CODE HERE
 	}
 	
 	
@@ -133,29 +238,125 @@ public class Board	{
 	*/
 	public int place(Piece piece, int x, int y) {
 		// flag !committed problem
-		if (!committed) throw new RuntimeException("place commit problem");
+		if (!committed) {
+			throw new RuntimeException("place commit problem");
+		}
+
+		if (x < 0 || y < 0) {
+			return PLACE_OUT_BOUNDS;
+		}
+
+		TPoint[] bodyOfPiece = piece.getBody();
+
+		// check if all spots are free and in bounds
+		for (int i = 0; i < bodyOfPiece.length; i++) {
+
+			int xSpot = x + bodyOfPiece[i].x;
+			int ySpot = y + bodyOfPiece[i].y;
+
+			if (xSpot >= width || ySpot >= height) {
+				return PLACE_OUT_BOUNDS;
+			}
+
+			if (getGrid(xSpot, ySpot)) { // true = no empty space
+				return PLACE_BAD;
+			}
+		} //for
+		
+		makeBackup();
+		committed = false;
+		placeMethodUsed = true;
+		
+		//ok, all spots are free. Lets place piece.
+		for (int i = 0; i < bodyOfPiece.length; i++) {
 			
-		int result = PLACE_OK;
+			int xSpot = x + bodyOfPiece[i].x;
+			int ySpot = y + bodyOfPiece[i].y;
+			
+			grid[xSpot][ySpot] = true;
+		}
 		
-		// YOUR CODE HERE
 		
-		return result;
+		makeHeightAndWidthArrays(); // refresh data
+		sanityCheck();
+		
+		/* check rows occupied by new piece for full row*/
+		for (int i = y; i < (y + piece.getHeight()); i++) {
+			if (widthsOfRows[i] == width) {
+				return PLACE_ROW_FILLED;
+			}
+		}
+		
+		return PLACE_OK;
+		
 	}
-	
 	
 	/**
 	 Deletes rows that are filled all the way across, moving
 	 things above down. Returns the number of rows cleared.
 	*/
 	public int clearRows() {
+		if (!committed && !placeMethodUsed) {
+			throw new RuntimeException("place commit problem");
+		}
+		placeMethodUsed = false; //sets marker to false, so can't be call again
+		committed = false;
+		
+		makeHeightAndWidthArrays(); //just in case
+		
 		int rowsCleared = 0;
-		// YOUR CODE HERE
+		int from = 0;
+		int to = 0;
+		while (from < height) {
+			
+			if (widthsOfRows[from] == width) {//if row is full
+				from++; //skip this line and take from next
+				rowsCleared++; 
+			}
+			
+			if (from == to) {//if no copy required
+				from++;
+				to++;
+				continue;
+			}
+			
+			copyRow(from, to); //move row down
+			
+		
+			from++;
+			to++;			
+		}
+		
+		if (rowsCleared > 0) { //refresh widths and heights
+			makeHeightAndWidthArrays();
+		}
+		
 		sanityCheck();
 		return rowsCleared;
 	}
 
+	/**
+	 * Copy row of grid
+	 * @param fromNofRow  number of row to copy from
+	 * @param toNofRow	  destination
+	 */
+	private void copyRow(int fromNofRow, int toNofRow) {
+		for (int x = 0; x < grid.length; x++) {
+			grid[x][toNofRow] = grid[x][fromNofRow]; 
+		}
+	}
 
 
+	private void makeBackup() {		
+		//make deep copy
+		for (int i = 0; i < grid.length; i++) {
+			 System.arraycopy(grid[i], 0, buGrid[i], 0, grid[i].length); 
+		}
+		
+		System.arraycopy(widthsOfRows, 0, buWidthsOfRows, 0, widthsOfRows.length);
+		System.arraycopy(heightsOfCols, 0, buHeightsOfCols, 0, heightsOfCols.length);
+	}
+	
 	/**
 	 Reverts the board to its state before up to one place
 	 and one clearRows();
@@ -164,7 +365,27 @@ public class Board	{
 	 See the overview docs.
 	*/
 	public void undo() {
-		// YOUR CODE HERE
+		
+		if (committed == true) {
+			return;
+		}
+		
+		if (buGrid == null) {
+			throw new RuntimeException("No source for backup");
+		}
+		
+		committed = true;
+
+		//make deep copy
+		for (int i = 0; i < buGrid.length; i++) {
+			 System.arraycopy(buGrid[i], 0, grid[i], 0, buGrid[i].length); 
+		}
+		
+		System.arraycopy(buWidthsOfRows, 0, widthsOfRows, 0, buWidthsOfRows.length);
+		System.arraycopy(buHeightsOfCols, 0, heightsOfCols, 0, buHeightsOfCols.length);
+		
+		sanityCheck();
+		
 	}
 	
 	
@@ -172,6 +393,7 @@ public class Board	{
 	 Puts the board in the committed state.
 	*/
 	public void commit() {
+		placeMethodUsed = false;
 		committed = true;
 	}
 
--- HW2Tetris/tetris/BoardTest.java
+++ HW2Tetris/tetris/BoardTest.java
@@ -7,6 +7,22 @@ import org.junit.*;
 public class BoardTest {
 	Board b;
 	Piece pyr1, pyr2, pyr3, pyr4, s, sRotated;
+	
+	/*
+	 * Board class checklist:
+	 * Board(int, int)  -- CHECK
+	 * getWidth() -- CHECK
+	 * getHeight() -- CHECK
+	 * getMaxHeight() -- CHECK
+	 * sanityCheck() -- CHECK (if DEBUG == true)
+	 * dropHeight(Piece, int) 
+	 * getColumnHeight(int) -- CHECK
+	 * getRowWidth(int) -- CHECK
+	 * getGrid(int, int) -- CHECK
+	 * place(Piece, int, int) -- CHECK
+	 * clearRows() -- CHECK
+	 * undo() -- CHECK
+	 */
 
 	// This shows how to build things in setUp() to re-use
 	// across tests.
@@ -50,11 +66,179 @@ public class BoardTest {
 		assertEquals(4, b.getColumnHeight(1));
 		assertEquals(3, b.getColumnHeight(2));
 		assertEquals(4, b.getMaxHeight());
+		
+		/* --> my tests*/
+		
+		//test clear rows
+		b.clearRows();
+		assertEquals(0, b.getColumnHeight(0));
+		assertEquals(3, b.getColumnHeight(1));
+		assertEquals(2, b.getColumnHeight(2));
+		assertEquals(3, b.getMaxHeight());
+		
+		//undo to the state of last commit
+		b.undo(); 
+		assertEquals(1, b.getColumnHeight(0));
+		assertEquals(2, b.getColumnHeight(1));
+		assertEquals(2, b.getMaxHeight());
+		assertEquals(3, b.getRowWidth(0));
+		assertEquals(1, b.getRowWidth(1));
+		assertEquals(0, b.getRowWidth(2));
+		
+		
+		
+		
 	}
 	
 	// Make  more tests, by putting together longer series of 
 	// place, clearRows, undo, place ... checking a few col/row/max
 	// numbers that the board looks right after the operations.
 	
+	@Test
+	public void testCase3() {
+		Board b2 = new Board(5, 5); 
+		
+		//System.out.println(b2.toString());
+		
+		//test new board		
+		assertEquals(0, b2.getColumnHeight(0));
+		assertEquals(0, b2.getColumnHeight(1));
+		assertEquals(0, b2.getMaxHeight());
+		assertEquals(0, b2.getRowWidth(0));
+		assertEquals(0, b2.getRowWidth(1));
+		assertEquals(0, b2.getRowWidth(2));
+		
+		b2.place(s, 0, 0);
+		b2.commit();
+		//System.out.println(b2.toString());
+		
+		/*
+		 * current state
+		    |     |
+			|     |
+			|     |
+			| ++  |
+			|++   |
+			-------
+		 */
+		
+
+		//check that undo doesn't do anything
+		b2.undo();
+		assertEquals(1, b2.getColumnHeight(0));
+		assertEquals(2, b2.getColumnHeight(1));
+		assertEquals(2, b2.getRowWidth(0));
+		assertEquals(2, b2.getRowWidth(1));
+		
+	}
 	
+	@Test
+	public void testDropHeight() {
+		Board b3 = new Board(3, 6); 
+		
+		b3.place(pyr1, 0, 0);
+		b3.commit();
+		
+		/*
+		System.out.println(b3);
+		|   |
+		|   |
+		|   |
+		|   |
+		| + |
+		|+++|
+		-----
+		*/
+		
+		assertEquals(2, b3.getColumnHeight(1)); //just checking
+
+		assertEquals(2, b3.dropHeight(pyr3, 0)); // pyr3 is upside down pyr1
+
+	}
+	
+	/*
+	 * dropHeight() should throw e if placed not in bounds
+	 */
+	@Test(expected=RuntimeException.class)
+	public void testDropHeightException() {
+		Board b3 = new Board(3, 6); 
+		
+		b3.place(pyr1, 0, 0);
+		b3.commit();
+		
+		/*
+		System.out.println(b3);
+		|   |
+		|   |
+		|   |
+		|   |
+		| + |
+		|+++|
+		-----
+		*/
+		
+
+		assertEquals(2, b3.dropHeight(pyr3, 1)); // pyr3 is upside down pyr1
+	}
+	
+	@Test
+	public void testClearRows() {
+		Board b3 = new Board(3, 6);
+		
+		b3.place(pyr1, 0, 0);
+		b3.commit();
+		
+		/*
+		System.out.println(b3);
+		|   |
+		|   |
+		|   |
+		|   |
+		| + |
+		|+++|
+		-----
+		*/
+		
+		
+		b3.clearRows();
+		b3.commit();
+		
+		/* now
+		 	|   |
+			|   |
+			|   |
+			|   |
+			|   |
+			| + |
+			-----
+		 */
+		assertEquals(0, b3.getColumnHeight(0));
+		assertEquals(1, b3.getColumnHeight(1));
+		assertEquals(1, b3.getRowWidth(0));
+		assertEquals(0, b3.getRowWidth(1));
+		
+		b3.place(pyr4, 0, 0);
+		b3.commit();
+		
+		/*System.out.println(b3);
+		 	|   |
+			|   |
+			|   |
+			|+  |
+			|++ |
+			|++ |
+			-----
+		 */
+		
+		//check how it works when there is no full rows
+		b3.clearRows(); 
+		
+		assertEquals(3, b3.getColumnHeight(0));
+		assertEquals(2, b3.getColumnHeight(1));
+		assertEquals(0, b3.getColumnHeight(2));
+		assertEquals(2, b3.getRowWidth(0));
+		assertEquals(1, b3.getRowWidth(2));
+		
+		
+	}
 }

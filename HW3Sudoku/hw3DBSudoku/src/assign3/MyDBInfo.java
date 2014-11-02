4b079d8094f65cf6160d40ef5879a35ed63c8c8d
Andrew Baliushin <uo4ever@mail.ru>
2014-11-02 10:06:50 +0400

    HW3 Provided frame

---
+++ HW3Sudoku/hw3DBSudoku/src/assign3/MyDBInfo.java
@@ -0,0 +1,16 @@
+package assign3;
+
+/*
+ * CS108 Student: This file will be replaced when we test your code. So, do not add any of your
+ * assignment code to this file. Also, do not modify the public interface of this file.
+ * Only change the public MyDBInfo constants so that it works with the database login credentials 
+ * that we emailed to you.
+ */
+public class MyDBInfo {
+	
+	public static final String MYSQL_USERNAME = "ccs108yourStudentID";
+	public static final String MYSQL_PASSWORD = "";
+	public static final String MYSQL_DATABASE_SERVER = "mysql-user.stanford.edu";
+	public static final String MYSQL_DATABASE_NAME = "c_cs108_yourStudentID";
+
+}
